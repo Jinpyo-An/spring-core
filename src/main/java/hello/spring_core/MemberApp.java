b086eb8f0448c4313bf8d57a5c487eb234f1d8f2
Jinpyo An <dkswlsvy3312@gmail.com>
2025-02-28 16:57:52 +0900

    객체 지향 원리 적용

--- src/main/java/hello/spring_core/MemberApp.java
+++ src/main/java/hello/spring_core/MemberApp.java
@@ -3,15 +3,21 @@ package hello.spring_core;
 import hello.spring_core.member.Grade;
 import hello.spring_core.member.Member;
 import hello.spring_core.member.MemberService;
-import hello.spring_core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        final MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
-
 
         Member findMember = memberService.findMember(1L);
         System.out.println("member = " + member.getName());
--- src/main/java/hello/spring_core/OrderApp.java
+++ src/main/java/hello/spring_core/OrderApp.java
@@ -3,22 +3,27 @@ package hello.spring_core;
 import hello.spring_core.member.Grade;
 import hello.spring_core.member.Member;
 import hello.spring_core.member.MemberService;
-import hello.spring_core.member.MemberServiceImpl;
 import hello.spring_core.order.Order;
 import hello.spring_core.order.OrderService;
-import hello.spring_core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        final MemberService memberService = appConfig.memberService();
+//        final OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
-        final Member member = new Member(memberId, "memberA", Grade.VIP);
+        Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        final Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
 
         System.out.println("order = " + order);
     }
